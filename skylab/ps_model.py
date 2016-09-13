20c9043872e8f8ad33d0e2ec0c7c650331ca1b93
tmenne <thorben.menne@tu-dortmund.de>
2016-09-13 12:49:27 +0200

    Added map chaching function as a replacement for __call__, because   I don't want to overwrite psLLH __init__ for a single line   changing.

--- skylab/ps_model.py
+++ skylab/ps_model.py
@@ -25,11 +25,13 @@ _N = 0
     _N = _N
     _cached_maps = _cached_maps
 
-
-    def __call__(self, exp, mc, livetime, src, **kwargs):
+    # INTERNAL METHODS
+    def _cache_maps(self, exp, mc, src):
         r"""
-        Cache maps before calling the super class.
+        Cache maps for fast signal calculation.
 
+        exp, mc : record arrays
+            Data, MC from psLLH.
         src : record array
             Record array containing the source information. Needed fields are
             sigma : array
@@ -41,17 +43,16 @@ _N = 0
         """
         # The first _N maps are always exp maps
         self._N = len(exp)
+
         # Cache maps with every exp/mc event sigma
         added_map = self._add_weighted_maps(src["sigma"], src["normw"])
         sigma = np.append(exp["sigma"], mc["sigma"])
         logger.info("Start caching exp and mc maps. This may take a while.")
         self.cached_maps = self._convolve_maps(added_map, sigma)
-        # Do the rest in the super class
-        super(HealpyLLH, self).__call__(exp, mc, livetime, **kwargs)
+        logger.info("Done caching exp and mc maps.")
+
         return
 
-
-    # INTERNAL METHODS
     def _convolve_maps(self, m, sigma):
         """
         This function does:
