116ddae10ed7c1d8f1cae8ef93043a779085da33
Thorben Menne <thorben.menne@tu-dortmund.de>
2016-11-21 09:02:23 +0100

    Fixed typo and strucutre in skylab/__init__.py.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
-# __init__.py added to use skylab as is as a submodule of python_modules.
-# import needs from anapymods.skylab.skylab import <what_you_need>
-# The skylab folder is a git submodule so it can be version controlled and
-# kept up to data with the origin from gtihub.com/coenders/skylab
+# coding:utf8
+
+"""
+__init__.py added to use skylab as is as a submodule of python_modules.
+import needs from anapymods.skylab.skylab import <what_you_need>
+The skylab folder is a git submodule so it can be version controlled and
+kept up to data with the origin from github.com/coenders/skylab.
+"""
